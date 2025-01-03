a0228ce884b6ba4ee84ddfea95e1491d718fc54b
Abisheksensi <jayathilakeabishek@gmail.com>
2025-01-03 21:11:30 +0530

    navbar update

--- src/Components/Nvbar.jsx
+++ src/Components/Nvbar.jsx
@@ -1,1 +1,29 @@
+import React from "react";
+import "./NavBar.css"; // Optional CSS file for styling
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <a href="/">MyWebsite</a>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
 

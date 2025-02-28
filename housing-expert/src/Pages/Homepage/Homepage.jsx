bb560640fda0e4161608ec6b5f64e005eb6816d7
Abisheksensi <jayathilakeabishek@gmail.com>
2025-02-28 16:17:44 +0530

    Add homepage sections: Hero, Gallery, Features, Trends, Map, and Footer

--- housing-expert/src/Pages/Homepage/Homepage.jsx
+++ housing-expert/src/Pages/Homepage/Homepage.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import HeroSec from "Sections/Homepage/HeroSec/HeroSec";
+import GallerySec from "Sections/Homepage/GallerySec/GallerySec";
+import FeaturesSec from "Sections/Homepage/FeaturesSec/FeaturesSec";
+import TrendsSec from "Sections/Homepage/TrendsSec/TrendsSec";
+import MapSec from "Sections/Homepage/MapSec/MapSec";
+import Footer from "Sections/Homepage/Footer/Footer";
 
 export class Homepage extends Component {
   static propTypes = {};
 
   render() {
-    return <div>Homepage</div>;
+    return (
+      <div className="homepage">
+        <HeroSec />
+
+        <FeaturesSec />
+
+        <GallerySec />
+
+
+        <TrendsSec />
+
+        <MapSec />
+
+        <Footer />
+      </div>
+    );
   }
 }
 
---
+++ housing-expert/src/Sections/Homepage/TrendsSec/TrendsSec.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrendsSec = () => {
+  return <div>TrendsSec</div>;
+};
+
+export default TrendsSec;

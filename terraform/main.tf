8a3ae3422e6731a5165e4fbcf59688e00e37a65a
Abisheksensi <jayathilakeabishek@gmail.com>
2026-02-13 03:34:32 +0530

    Add EKS Node Group for worker nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,3 +115,56 @@ resource "aws_instance" "ansible_target" {
     Name = "Ansible-Target-Server"
   }
 }
+
+# --- EKS Node Group ---
+resource "aws_iam_role" "eks_node_role" {
+  name = "housing-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_policy_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_policy_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_policy_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_eks_node_group" "housing_nodes" {
+  cluster_name    = aws_eks_cluster.housing_cluster.name
+  node_group_name = "housing-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  # Use exact same subnets as cluster
+  subnet_ids      = [aws_subnet.public_subnet.id, aws_subnet.public_subnet_2.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"] # t3.medium is standard for EKS nodes
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_node_policy_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks_node_policy_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks_node_policy_AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
